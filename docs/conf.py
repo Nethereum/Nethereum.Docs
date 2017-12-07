2feec3fdb4754aca122ec7884f45fa384fda37c1
gael blanchemain <gael.blanchemain@protonmail.com>
2017-12-07 15:14:24 -0500

    attempt at commiting just index.md

--- docs/conf.py
+++
@@ -1,8 +0,0 @@
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-    '.workbook': CommonMarkParser
-}
-
-source_suffix = ['.workbook', '.md']
--- docs/ethereum-and-clients/parity.md
+++
@@ -1,2 +0,0 @@
-#Parity
-Parity is an Ethereum client based on the RUST language. https://github.com/paritytech/parity/wiki.
